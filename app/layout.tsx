c613f0acb7457b1967e6830a536107b203d4a0e8
colin <ColinWang98@users.noreply.github.com>
2026-05-08 07:15:08 +0800

    Add pixel Hong Kong favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "HK Spatial Story",
-  description: "Hong Kong panorama fragment story prototype"
+  description: "Hong Kong panorama fragment story prototype",
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg"
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
